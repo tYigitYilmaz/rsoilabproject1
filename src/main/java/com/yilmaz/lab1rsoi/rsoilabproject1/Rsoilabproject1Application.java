a738f8d9980c0e08bf55c836ff123c5ce3eae531
Yigit Yilmaz <tahayigityilmaz@gmail.com>
2018-10-03 08:42:30 +0300

    MainController / Server port, #local running.

---
+++ src/main/java/com/yilmaz/lab1rsoi/rsoilabproject1/Rsoilabproject1Application.java
@@ -0,0 +1,12 @@
+package com.yilmaz.lab1rsoi.rsoilabproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rsoilabproject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rsoilabproject1Application.class, args);
+    }
+}
---
+++ src/main/java/com/yilmaz/lab1rsoi/rsoilabproject1/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.yilmaz.lab1rsoi.rsoilabproject1.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @RequestMapping("/{name}")
+    public String helloUser(@PathVariable String name) {
+        return "Hello " + name;
+    }
+}
